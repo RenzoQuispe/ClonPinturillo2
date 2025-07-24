bd6ef7966f37c7bff82e2ec32210f6860c8cef79
RenzoQuispe <renzo123cd@gmail.com>
2025-07-24 01:46:05 -0500

    estilizar contenedores en LobbyPage.jsx (lista de salas y chat general), no son funcionales aun

--- frontend/src/pages/LobbyPage.jsx
+++ frontend/src/pages/LobbyPage.jsx
@@ -1,20 +1,42 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import { Send } from "lucide-react";
 
 function LobbyPage({ setCodigoMesa, setNumMesa, setUsername, setCurrentPage, username, numMesa, codigoMesa }) {
-    const handleHomePage = () => {
-        setUsername(username)
-        setCurrentPage('home')
-    }
     return (
         <div style={{ backgroundColor: '#336767', height: '100dvh' }} className="overflow-auto flex flex-col items-center">
             {/*encabezado */}
             <Header idioma="es" setCurrentPage={setCurrentPage} setUsername={setUsername} username={username} />
-
             {/*Contenido */}
-            <div className="min-h-[450px] h-[515px] w-[410px] bg-cover bg-center mt-15 mb-15">
+            <div className="flex justify-between space-x-5 mt-3 mb-3">
                 {/* lista de salas */}
+                <div style={{ border: "5px solid #a09c34" }} className="bg-gray-200 rounded-3xl h-[600px] w-[750px] text-black p-2 flex flex-col justify-between">
+                    <div className="overflow-y-auto flex-1 text-black">
+                        <div style={{ background: "#c8c454" }} className="rounded-tl-2xl rounded-tr-2xl h-[30px] w-[720px] text-white text-2xl font-bold">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mesas: 80 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Usuarios: 712</div>
+                        <ul className="space-y-1 text-black">
+                            
+                        </ul>
+                    </div>
+                </div>
                 {/* chat general */}
+                <div style={{ border: "5px solid #a09c34" }} className="bg-gray-200 rounded-3xl h-[600px] w-[400px] text-black p-2 flex flex-col justify-between">
+                    <div className="overflow-y-auto flex-1 text-black">
+                        <ul className="space-y-1 text-black">
+                            
+                        </ul>
+                    </div>
+                    <div className="mt-2 flex">
+                        <input
+                            className="flex-1 p-1 ml-1 rounded-sm text-black border bg-gray-300"
+                        />
+                        <button
+                            style={{ background: "#336767" }}
+                            className="ml-2 px-2 w-[38px] text-white font-bold rounded-sm hover:brightness-110"
+                        >
+                            <Send size={18} />
+                        </button>
+                    </div>
+                </div>
             </div>
             {/*pie de pagina */}
             <Footer />
