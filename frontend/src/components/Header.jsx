884c94e1ee7fe7c92b658d692ccd4a56ed2cc5be
RenzoQuispe <renzo123cd@gmail.com>
2025-07-24 01:21:24 -0500

    actualizar Header.jsx para futuro estado segun idioma

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,13 @@
 import { useState, useEffect, useRef } from "react";
 
-function Header() {
+function Header({ idioma, setCurrentPage, setUsername, username }) {
     const [isOpen, setIsOpen] = useState(false);
     const dropdownRef = useRef(null);
+
+    const handleHomePage = () => {
+        setUsername(username)
+        setCurrentPage('home')
+    }
 
     // Cerrar el menú si se hace clic fuera
     useEffect(() => {
@@ -15,40 +20,53 @@ function Header() {
         return () => window.removeEventListener("click", handleClickOutside);
     }, []);
 
-    return (
-        <div style={{ backgroundColor: '#484444' }} className="relative w-full h-[75px] flex items-center justify-center">
-            {/* Selector de idioma */}
-            <div className="absolute inline-block text-left left-5 rounded-xl" ref={dropdownRef}>
-                <button
-                    onClick={() => setIsOpen(!isOpen)}
-                    style={{ backgroundColor: '#5c5b5b' }} 
-                    className="flex items-center font-bold gap-2 w-[200px] bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-600 hover:brightness-150 cursor-pointer"
-                >
-                    🌐  ESPAÑOL
-                    <svg className="w-4 h-4 ml-11" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
-                    </svg>
-                </button>
+    if (!idioma) {
+        return (
+            <div style={{ backgroundColor: '#484444' }} className="relative w-full h-[75px] flex items-center justify-center">
+                {/* Selector de idioma */}
+                <div className="absolute inline-block text-left left-5 rounded-xl" ref={dropdownRef}>
+                    <button
+                        onClick={() => setIsOpen(!isOpen)}
+                        style={{ backgroundColor: '#5c5b5b' }}
+                        className="flex items-center font-bold gap-2 w-[200px] bg-gray-700 text-white px-4 py-2 rounded hover:bg-gray-600 hover:brightness-150 cursor-pointer"
+                    >
+                        🌐  ESPAÑOL
+                        <svg className="w-4 h-4 ml-11" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+                        </svg>
+                    </button>
 
-                {isOpen && (
-                    <div className="absolute z-10 mt-2 w-40 bg-gray-700 text-white rounded shadow-lg">
-                        <ul className="py-1">
-                            {[
-                                "DANSK", "DEUTSCH", "ENGLISH", "ESPAÑOL", "FRANÇAIS", "ITALIANO", "MAGYAR",
-                                "NEDERLANDS", "NORWEGIAN", "POLSKI", "PORTUGUÊS", "ROMÂNĂ", "SVENSKA", "TÜRKÇE"
-                            ].map(lang => (
-                                <li key={lang}>
-                                    <a href="#" className="block px-4 py-2 hover:bg-gray-600">{lang}</a>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
+                    {isOpen && (
+                        <div className="absolute z-10 mt-2 w-40 bg-gray-700 text-white rounded shadow-lg">
+                            <ul className="py-1">
+                                {[
+                                    "DANSK", "DEUTSCH", "ENGLISH", "ESPAÑOL", "FRANÇAIS", "ITALIANO", "MAGYAR",
+                                    "NEDERLANDS", "NORWEGIAN", "POLSKI", "PORTUGUÊS", "ROMÂNĂ", "SVENSKA", "TÜRKÇE"
+                                ].map(lang => (
+                                    <li key={lang}>
+                                        <a href="#" className="block px-4 py-2 hover:bg-gray-600">{lang}</a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
+                {/* Logo */}
+                <img src="/pinturillo2_tipo2.png" alt="Logo" className="w-[250px] h-[75px] flex justify-center" />
             </div>
-            {/* Logo */}
-            <img src="/pinturillo2_tipo2.png" alt="Logo" className="w-[250px] h-[75px] flex justify-center" />
-        </div>
-    );
+        );
+    } else {
+        return (
+            <div style={{ backgroundColor: '#484444' }} className="relative w-full h-[75px] flex items-center justify-center">
+                <button className=" absolute inline-block text-left left-5 ml-10 hover:brightness-110" onClick={handleHomePage}>
+                    <img src="/atras2.png" alt="Volver" className="h-[48px] w-[48px]" />
+                </button>
+                {/* Logo */}
+                <img src="/pinturillo2_tipo2.png" alt="Logo" className="w-[250px] h-[75px] flex justify-center" />
+            </div>
+        );
+    }
+
 }
 
 export default Header;
