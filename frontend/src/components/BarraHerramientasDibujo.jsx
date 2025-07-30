1497487fbfdc921352df67ae8145c91d3b0d52f5
RenzoQuispe <renzo123cd@gmail.com>
2025-07-29 23:18:26 -0500

    estilizar componente BarraHerramientasDibujo.jsx

--- frontend/src/components/BarraHerramientasDibujo.jsx
+++ frontend/src/components/BarraHerramientasDibujo.jsx
@@ -5,18 +5,32 @@ export default function BarraHerramientasDibujo({
     onSaltarTurno
 }) {
     const colores = [
-        "#ffffff", "#ff0000", "#ffff00", "#00ff00", "#00ffff",
-        "#0000ff", "#ff00ff", "#808080", "#000000"
+        "#000000", // negro
+        "#808080", // gris
+        "#FFFFFF", // blanco
+        "#800000", // marrón oscuro
+        "#FF0000", // rojo
+        "#FFA500", // naranja
+        "#FFFF00", // amarillo
+        "#008000", // verde
+        "#00FF00", // verde claro
+        "#00FFFF", // cian
+        "#0000FF", // azul
+        "#000080", // azul oscuro
+        "#800080", // púrpura
+        "#FF00FF", // fucsia
+        "#FFC0CB", // rosado
+        "#A52A2A", // marrón
     ];
-    const grosores = [3, 8, 12];
+
+    const grosores = [3, 8, 14];
 
     return (
-        <div className="h-14 w-100 flex items-center bg-[#cc6] rounded-br-xl shadow-lg">
+        <div className="h-12 w-100 flex items-center bg-[#cc6] rounded-br-xl shadow-lg">
             {/* botón limpiar campo de dibujo */}
-            <button onClick={onLimpiar} className="w-8 h-8 ml-5 mr-3 bg-white border border-black flex items-center justify-center">
-                <img src="/fondo_campo_dibujo.png" alt="Limpiar" className="w-6 h-6" />
+            <button onClick={onLimpiar} style={{border: "2px solid #a09c34"}} className="w-10 h-10 ml-5 mr-3 bg-white flex items-center justify-center rounded-md">
+                <img src="/limpiar_canva.png" alt="Limpiar" className="w-6 h-6" />
             </button>
-
             {/* Paleta de colores */}
             <div className="grid grid-cols-8 gap-0 w-fit">
                 {colores.map((color) => (
@@ -28,14 +42,14 @@ export default function BarraHerramientasDibujo({
                     />
                 ))}
             </div>
-
             {/* Grosor */}
             <div className="ml-3 flex gap-1">
                 {grosores.map((grosor) => (
                     <button
+                        style={{border: "2px solid #a09c34"}}
                         key={grosor}
                         onClick={() => onGrosorChange(grosor)}
-                        className="w-8 h-8 relative border border-black bg-[#eee]"
+                        className="w-8 h-8 relative bg-[#eee] rounded-md"
                     >
                         <div
                             className="absolute rounded-full bg-black"
@@ -49,9 +63,8 @@ export default function BarraHerramientasDibujo({
                     </button>
                 ))}
             </div>
-
             {/* saltar turno */}
-            <button onClick={onSaltarTurno} className="ml-3 w-8 h-8 bg-[#cc6] text-xl font-bold">X</button>
+            <button style={{color: "#a09c34"}} onClick={onSaltarTurno} className="ml-3 w-8 h-8 bg-[#cc6] text-3xl font-bold">X</button>
         </div>
     );
 }
